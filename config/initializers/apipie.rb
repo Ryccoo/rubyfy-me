689b576e48edff6f44b077e01f9829f8ff274144
Richard Ludvigh <richard.ludvigh@gmail.com>
2014-12-31 09:56:24 +0100

    Remove api version from initializer, add link to navigation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,4 @@
 Apipie.configure do |config|
-  config.api_version             = "v1"
   config.app_name                = "RubyBench"
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/apidoc"
